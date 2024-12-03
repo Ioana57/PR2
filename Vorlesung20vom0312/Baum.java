9c158b31c45dc729d6be245005bfeb869f6fe1fd
Ioana P. <3015825@stud.hs-mannheim.de>
2024-12-03 16:51:06 +0100

    vorlesung: bäume

--- Vorlesung20vom0312/Baum.java
+++ Vorlesung20vom0312/Baum.java
@@ -8,65 +8,67 @@ public class Baum {
         System.out.println("Leerer Baum angelegt.");
     }
 
-    public void insert(int neu){
-        if(wurzel == null){
+    public void insert(int neu) {
+        if (wurzel == null) {
             wurzel = new Knoten(neu);
-        }else {
+        } else {
             wurzel.add(neu);
         }
     }
 
-    public void inorderTraversierung(){
-        if(wurzel == null){
+    public void inorderTraversierung() {
+        if (wurzel == null) {
             System.out.println("Baum ist leer.");
-        }else if (wurzel != null){
+        } else if (wurzel != null) {
             System.out.println("Inorder Traversierung: ");
             wurzel.inorder();
         }
     }
 
-}
 
     class Knoten {
         private int wert;
         private Knoten links, rechts;
+        private int htb;
 
         public Knoten(int wert) {
             this.wert = wert;
             this.links = null;
             this.rechts = null;
+            this.htb = 0;
 
             System.out.println(wert + " als Knoten angelegt.");
         }
 
         public void add(int neu) {
-            if(neu == this.wert){
+            if (neu == this.wert) {
                 throw new IllegalArgumentException("Wert darf nicht doppelt gespeichert werden.");
             }
 
-            if(neu < this.wert){              // nach links schauen
-                if(links == null){
+            if (neu < this.wert) {              // nach links schauen
+                if (links == null) {
                     links = new Knoten(neu);
-                } else if (links !=null){
+                } else if (links != null) {
                     links.add(neu);
                 }
-            } else if (neu > this.wert){     // nach rechts schauen
+            } else if (neu > this.wert) {     // nach rechts schauen
                 if (rechts == null) {
                     rechts = new Knoten(neu);
-                } else if (rechts !=null){
+                } else if (rechts != null) {
                     rechts.add(neu);
                 }
 
-        }
-    } // end add
+            }
+        } // end add
 
-        public void inorder(){
-            if(links != null) {
+        public void inorder() {
+            if (links != null) {
                 links.inorder();
             }
             System.out.print(this.wert + " ");
-            if(rechts != null) {
+            if (rechts != null) {
                 rechts.inorder();
             }
         }
-} // end Knoten
+    } // end Knoten
+} //end Baum
